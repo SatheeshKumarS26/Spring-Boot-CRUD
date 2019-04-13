5613cd84fa11eeee41fed92c00b23d39fae2b7e7
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-04-13 14:33:05 +0530

    adding my first class

---
+++ Server/src/main/java/com/example/test/demo/People.java
@@ -0,0 +1,16 @@
+package com.example.test.demo;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class People {
+    @Id @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+}
